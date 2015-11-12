03bdeb67af20558e98ddd7fe3ac359621c75d4ad
Hugh Chiou <hugh__@hotmail.com>
2015-11-12 22:31:39 +0800

    加入資料夾並安裝所需的NUGET套件

---
+++ angularMVC/App_Code/basepage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace angularMVC.App_Code
+{
+    public class basepage:System.Web.UI.Page
+    {
+
+    }
+}
